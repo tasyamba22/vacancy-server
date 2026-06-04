4d8466af580d4b077dc76f10e1784d0055accb53
Taisia <taisia.filippova.22@gmail.com>
2026-06-05 00:00:31 +0300

    feat: add generateRefreshToken method in JwtHelper

--- src/main/kotlin/com/vacancy/server/utils/JwtHelper.kt
+++ src/main/kotlin/com/vacancy/server/utils/JwtHelper.kt
@@ -21,4 +21,22 @@ object JwtHelper {
             .withExpiresAt(Date(System.currentTimeMillis() + expirationMs))
             .sign(Algorithm.HMAC256(secret))
     }
+
+    fun generateRefreshToken(
+        userId: Int,
+        secret: String,
+        issuer: String,
+        audience: String,
+        expirationDays: Long = 30
+    ): String {
+        val expirationMs = System.currentTimeMillis() + (expirationDays * 24 * 60 * 60 * 1000)
+
+        return JWT.create()
+            .withAudience(audience)
+            .withIssuer(issuer)
+            .withClaim("userId", userId)
+            .withClaim("type", "refresh")           // важно отличать
+            .withExpiresAt(java.util.Date(expirationMs))
+            .sign(Algorithm.HMAC256(secret))
+    }
 }
